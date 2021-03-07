8a46c37bdc432e37bfbb5c829605f7ee2270cb88
Lorena <lorena@proesc.com>
2021-03-07 18:53:18 -0300

    Criadas rotas de candidatos

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,12 @@
 const { Router } = require('express');
 const routes = Router();
+const CandidatesController = require('./controllers/CandidatesController');
 
 routes.get('/', (req, res) => {
 	return res.json({ status: true }).send();
 });
+
+routes.get('/candidates', CandidatesController.index);
+routes.post('/candidates', CandidatesController.store);
 
 module.exports = routes;
