78a5f78bd5f308f49ffb211ec6b99db473a76a5c
Lorena <lorena@proesc.com>
2021-03-08 21:54:02 -0300

    Refatorado teste de candidatos

--- __tests__/candidates.test.js
+++ __tests__/candidates.test.js
@@ -1,5 +1,7 @@
 const request = require('supertest');
 const app = require('../src/app');
+const Candidate = require('../src/models/Candidate');
+const mongoose = require('mongoose');
 
 describe('Testing the candidates API', () => {
     it('tests the base route and returns true for status', async () => {
@@ -14,12 +16,12 @@ describe('Testing the candidates API', () => {
     });
 
     it('tests the post new candidate endpoint and returns 200 for status', async () => {
-		const response = await supertest(app).post('/candidates').send({
-			city: 'Macapá',
-			experience: '2+ years',
+		const response = await request(app).post('/candidates').send({
+			city: 'Goiânia - GO',
+			experience: '0-1 years',
 			technologies: [
                 {
-                    name: 'Javascript',
+                    name: 'JavaScript',
                     is_main_tech: true
                 },
                 {
@@ -30,4 +32,21 @@ describe('Testing the candidates API', () => {
         });
 		expect(response.status).toBe(200);
     });
+
+    afterEach(async () => {
+      await Candidate.deleteOne({
+        city: 'Goiânia - GO'
+      })
+    })
+
+    afterAll(async(done) => {
+      try {
+        await mongoose.connection.close();
+        done()
+      } catch (error) {
+        console.log(error);
+        done()
+      }
+    })
+    
 });
