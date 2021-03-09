c582ac2a64f53aa2824dc1accb81311102f9ea65
Lorena <lorena@proesc.com>
2021-03-08 23:01:26 -0300

    Adicionados filtros

--- src/controllers/CandidatesController.js
+++ src/controllers/CandidatesController.js
@@ -3,12 +3,31 @@ const api = require('../services/api');
 
 module.exports = {
   async index(request, response) {
+    const filters = request.query;
+
     const candidates = await api.get(`/code_challenge.json`);
 
+    let filteredCandidates = candidates.data.candidates;
+
+    if(filters.city){
+      filteredCandidates = filteredCandidates.filter(candidate => candidate.city === filters.city);
+    }
+
+    if(filters.experience){
+      filteredCandidates = filteredCandidates.filter(candidate => candidate.experience === filters.experience);
+    }
+
+    if(filters.techs){
+      console.log(filters.techs)
+    }
+
+    if(filteredCandidates.length > 5){
+        filteredCandidates = filteredCandidates.slice(0,5);
+    }
     // if necessary to list the candidates from this api
     // const candidates = await Candidate.find();
 
-    return response.json(candidates.data.candidates);
+    return response.json(filteredCandidates);
   },
 
   async store(request, response) {
